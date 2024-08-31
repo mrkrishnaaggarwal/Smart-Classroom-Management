281fe6c964607de71a4322d2d5d73d6c74403e64
Nikhil Sharma <nikhil11072005@gmail.com>
2024-08-31 11:42:11 +0530

    routes

--- FRONTEND/src/Pages/ProfessorClasses.jsx
+++ FRONTEND/src/Pages/ProfessorClasses.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+async function saveImageToBase64() {
+  try {
+    // Read image file and convert to Base64
+    const image = fs.readFileSync("/Users/krishnaaggarwal/Desktop/Mac/ml/IMG_4864.jpg");
+    const base64Image = image.toString('base64');
+    
+    // Create a document to store Base64 image
+    const document = {
+      imageData: base64Image,
+      createdAt: new Date()
+    };
 
+    return document;
+  } catch (err) {
+    console.error('Error occurred while saving image:', err);
+  }
+}
 const ProfessorClasses = () => {
-  
+
   return (
     <div>
       ProfessorClasses
